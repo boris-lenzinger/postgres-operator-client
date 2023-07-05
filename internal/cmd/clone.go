c9f1f2bc9c0d80eff0f5bd905f002223e252a58f
boris <boris.lenzinger@gmail.com>
2023-07-05 14:44:34 +0200

    [debug] Still adding debug trace for enduser

--- internal/cmd/clone.go
+++ internal/cmd/clone.go
@@ -132,11 +132,14 @@ func newCloneCommand(config *internal.Config) *cobra.Command {
 					return errors.Wrap(err, "failed to dump objects before clone creation")
 				}
 				if additionalConfigPgBackrest.Name != "" {
+					fmt.Printf("Creating the additional configmap for pgbackrest with name %s...\n", additionalConfigPgBackrest.Name)
 					cm, err := clientK8s.CoreV1().ConfigMaps(targetNamespace).Create(context.TODO(), &additionalConfigPgBackrest, metav1.CreateOptions{})
 					if err != nil {
 						return errors.Wrap(err, "failed to create configmap for pgbackrest configuration before clone creation")
 					}
 					configMapsToDelete = append(configMapsToDelete, cm.Name)
+				} else {
+					fmt.Println("Not creating additional configmap for pgbackrest...")
 				}
 			}
 
